39f11eafce88376ae1212468f3025aad8084bf50
Masgee <akudankaucumanteman@gmail.com>
2024-11-29 16:23:46 +0700

    aa

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { useAuthStore } from '@/store/authStore';
 
 const apiClient = axios.create({
-    baseURL: 'http://localhost:3000/api',
+    baseURL: 'https://inventory-manajement-backend.vercel.app/api',
     headers: {
         'Content-Type': 'application/json',
     },
